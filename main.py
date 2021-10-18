871f9d3c50f5638d608ac0f8eb216a774374994b
Feldaria <54859411+Feldariia@users.noreply.github.com>
2021-10-17 20:45:52 -0400

    Version 2 push

--- main.py
+++ main.py
@@ -1,54 +1,54 @@
 # Name: Shannon Mong/Michelle Golden
 # Summary: Sequence Alignment
-# Date: 16 Oct 2021
-# Version: 1.0
+# Date: 17 Oct 2021
+# Version: 2.0
 # Class: SCS341
-# Time complexity: O(mn)
+# Time complexity: O(n!) *Can use Needleman-Wunsch for O(nm), but is not necessary for 6 items of data
+
+
+#Import packages
 from collections import deque
 from functools import partial
 
 
+#Ask user for input
+input = str(input("Please insert word to match: "))
+print("Starting Sequence Alignment on", input)
 
-def all_alignments(x, y):
-    """Return an iterable of all alignments of two
-    sequences.
 
-    x, y -- Sequences.
-    """
+# Alignment of two sequences
+def alignments(x, y):
+    # Recursion method for alignment sequences
+    def alignment_recursion(x, y):
 
-    def F(x, y):
-        """A helper function that recursively builds the
-        alignments.
-
-        x, y -- Sequence indices for the original x and y.
-        """
         if len(x) == 0 and len(y) == 0:
             yield deque()
 
-        scenarios = []
-        if len(x) > 0 and len(y) > 0:
-            scenarios.append((x[0], x[1:], y[0], y[1:]))
-        if len(x) > 0:
-            scenarios.append((x[0], x[1:], None, y))
-        if len(y) > 0:
-            scenarios.append((None, x, y[0], y[1:]))
 
-        # NOTE: "xh" and "xt" stand for "x-head" and "x-tail",
-        # with "head" being the front of the sequence, and
-        # "tail" being the rest of the sequence. Similarly for
-        # "yh" and "yt".
-        for xh, xt, yh, yt in scenarios:
-            for alignment in F(xt, yt):
-                alignment.appendleft((xh, yh))
+        #Appending sequences to search for a match
+        sequences = []
+        if len(x) > 0 and len(y) > 0:
+            sequences.append((x[0], x[1:], y[0], y[1:]))
+        if len(x) > 0:
+            sequences.append((x[0], x[1:], None, y))
+        if len(y) > 0:
+            sequences.append((None, x, y[0], y[1:]))
+
+        # Head: Front of Sequence
+        # Tail: Back of Sequence
+        for head1, tail1, head2, tail2 in sequences:
+            for alignment in alignment_recursion(tail1, tail2):
+                alignment.appendleft((head1, head2))
                 yield alignment
 
-    alignments = F(range(len(x)), range(len(y)))
+    alignments = alignment_recursion(range(len(x)), range(len(y)))
     return map(list, alignments)
 
+#Push alignments into a list
+list(alignments)
 
-list(all_alignments("CAT", "CT"))
 
-
+# Outputting the sequence alignments
 def print_alignment(x, y, alignment):
     print("".join(
         "-" if i is None else x[i] for i, _ in alignment
@@ -60,25 +60,22 @@ def print_alignment(x, y, alignment):
 
 x = "CAT"
 y = "CT"
-for alignment in all_alignments(x, y):
+for alignment in alignments(x, y):
     print_alignment(x, y, alignment)
     print()
 
+    #Scoring an alignment based on sequences
+    def alignment_score(x, y, match):
 
-    def alignment_score(x, y, alignment):
-        """Score an alignment.
-
-        x, y -- sequences.
-        alignment -- an alignment of x and y.
-        """
-        score_gap = -1
+        #Score variables
         score_same = +1
         score_different = -1
+        gap_penalty = -1
 
         score = 0
-        for i, j in alignment:
+        for i, j in match:
             if (i is None) or (j is None):
-                score += score_gap
+                score += gap_penalty
             elif x[i] == y[j]:
                 score += score_same
             elif x[i] != y[j]:
@@ -87,23 +84,25 @@ for alignment in all_alignments(x, y):
         return score
 
 
-    x = "CAT"
-    y = "CT"
-    alignment = [(0, 0), (1, None), (2, 1)]
-    alignment_score(x, y, alignment)
+
+    data = open("datafile.txt", "w")
+    items = ["Watch", "Cat", "Mouse", "Lay", "Sing", "Draw"]
+
+    x = items
+    y = items
+    match = [(0, 0), (1, None), (2, 1)]
+    alignment_score(x, y, match)
 
 
-def align_bf(x, y):
-    """Align two sequences, maximizing the
-    alignment score, using brute force.
-
-    x, y -- sequences.
-    """
+def align_calc(x, y):
     return max(
-        all_alignments(x, y),
+        alignments(x, y),
         key=partial(alignment_score, x, y),
     )
 
 
-align_bf("CAT", "CT")
-print_alignment("CAT", "CT", align_bf("CAT", "CT"))
+print(items)
+print(alignments)
+align_calc("CAT", "CT")
+print_alignment("CAT", "CT", align_calc("CAT", "CT"))
+
